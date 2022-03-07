de0b9e36c743c6f96fddc7e53e9982ef0e781fcb
Opilane TTHK <opilane@tthk.ee>
2022-03-07 10:08:14 +0200

    Add project files.

---
+++ Nelinurk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nelinurk
+{
+    public class Nelinurk
+    {
+        double pindala;
+        double korgus;
+        double laius;
+        double type;
+
+        public Nelinurk() { }
+        public Nelinurk(double korgus, double laius)
+        {
+            this.korgus = korgus;
+            this.laius = laius;
+        }
+        public double Korgus 
+        {
+            get
+            {
+                return korgus;
+            }
+            set
+            {
+                korgus = value;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nelinurk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("Write first side: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write second side: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Nelinurk abcd = new Nelinurk();
+            abcd.Korgus =
+
+        }
+    }
+}
